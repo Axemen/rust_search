96f4469b01a3a211a165961d3c4494b16c12b0d7
Jonathan Randolph <Jrandolph011@gmail.com>
2021-05-03 21:52:29 -0500

    adding fast_cosine_scores fn

--- search/src/lib.rs
+++ search/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
-use std::io;
 use std::fs;
+use std::io;
 
 pub mod index;
 pub mod preprocessing;
@@ -18,7 +18,7 @@ pub struct Search {
 pub trait Searcher {
     fn index_document(self, text: String);
     fn index_file(self, path: String) -> Result<(), io::Error>;
-    // fn fast_cosine_score(self, term: String) -> u32;
+    fn fast_cosine_scores(self, term: String) -> HashMap<i32, f32>;
     // fn lookup(self, terms: Vec<String>) -> Vec<String>;
     // fn remove(self, term: String) -> Option<()>;
 }
@@ -40,9 +40,21 @@ impl Searcher for Search {
         }
     }
 
-    fn index_file(self, path:String) -> Result<(), io::Error> {
+    fn index_file(self, path: String) -> Result<(), io::Error> {
         let text = fs::read_to_string(path).expect("Failed to read file");
         self.index_document(text);
         return Ok(());
+    }
+
+    fn fast_cosine_scores(self, term: String) -> HashMap<i32, f32> {
+        let mut scores: HashMap<i32, f32> = HashMap::new(); // document_id -> tfidf_score
+        let N = self.index.len() as f32; // Number of terms in the index
+        let df = self.index[&term].len() as f32; // Number of documents this term is in
+
+        for doc in &self.index[&term] {
+            let tfidf = doc.count as f32 * (N / df).ln() + 1.0;
+            *scores.entry(doc.document_id).or_insert(0.0) += tfidf;
+        }
+        return scores;
     }
 }
